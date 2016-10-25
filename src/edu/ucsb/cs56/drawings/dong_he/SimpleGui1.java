5976dc9d8ffb2221d5bea1c16723ee66bb0ff2a5
dong1016 <Dong@169-231-124-231.wireless.ucsb.edu>
2016-10-25 13:00:18 -0700

    changed button color/text

--- src/edu/ucsb/cs56/drawings/dong_he/SimpleGui1.java
+++ src/edu/ucsb/cs56/drawings/dong_he/SimpleGui1.java
@@ -20,9 +20,9 @@ public class SimpleGui1 {
     public static void main (String[] args) {
 	JFrame frame = new JFrame() ;
 	
-	JButton button = new JButton("Click me") ;
+	JButton button = new JButton("INSERT CREATIVE LABEL HERE") ;
 	
-	java.awt.Color myColor = new java.awt.Color(204,255,000);   // R, G, B values.
+	java.awt.Color myColor = new java.awt.Color(000,255,255);   // R, G, B values.
 	button.setBackground(myColor);
 	button.setOpaque(true);
 	
