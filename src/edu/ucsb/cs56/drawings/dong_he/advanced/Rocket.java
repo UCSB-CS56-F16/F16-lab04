086978bc9edd730c4c7583b2bdd83835986894ff
dong1016 <Dong@169-231-117-165.wireless.ucsb.edu>
2016-10-25 19:38:44 -0700

    work

---
+++ src/edu/ucsb/cs56/drawings/dong_he/advanced/Rocket.java
@@ -0,0 +1,107 @@
+package edu.ucsb.cs56.drawings.dong_he.advanced;
+import java.awt.geom.GeneralPath; // combinations of lines and curves
+import java.awt.Shape; // general class for shapes
+
+import edu.ucsb.cs56.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
+/**
+
+Size of frame is 640 * 480
+width * height
+
+**/
+
+/**
+   A modified CoffeeCup class that is used to create a rocket ship. 
+      
+   @author Dong He 
+   @version for CS56, F16, UCSB
+   
+*/
+
+public class Rocket extends GeneralPathWrapper implements Shape
+{   
+    /**
+     * Constructor for objects of class Rocket
+     */
+    public Rocket(double x, double y, double width, double height)
+    {
+	
+        // Specify the upper left corner, and the 
+        //  width and height of the original points used to 
+        //  plot the *hard-coded* rocket
+        
+        final double ORIG_ULX = 100.0; 
+        final double ORIG_ULY = 100.0; 
+        final double ORIG_HEIGHT = 600.0;
+        final double ORIG_WIDTH = 200.0;
+        
+        GeneralPath leftSide = new GeneralPath();
+	
+        // left of the rocket
+	
+        leftSide.moveTo(100,600);
+        leftSide.lineTo(100,100);
+        
+        GeneralPath topAndBottom = new GeneralPath();
+       
+        topAndBottom.moveTo(100,100);
+        topAndBottom.lineTo(200,100); // top of rocket
+        
+        topAndBottom.moveTo(100,600);
+        topAndBottom.lineTo(200,600); // bottom of rocket
+        
+        // left fin of the rocket
+        GeneralPath leftFin = new GeneralPath();
+        
+        leftFin.moveTo(100, 600);
+        leftFin.lineTo(50,600);
+        leftFin.lineTo(100,500);
+        
+        //right fin of the rocket
+        Shape rightFin = ShapeTransforms.horizontallyFlippedCopyOf(leftFin);
+        
+        rightFin = ShapeTransforms.translatedCopyOf(rightFin, 200.0, 0.0);
+       
+        //top of the rocket
+        GeneralPath topOfRocket = new GeneralPath();
+        topOfRocket.moveTo(100,100);
+        topOfRocket.lineTo(150,25);
+        topOfRocket.lineTo(200,100);
+        
+        Shape rightSide = ShapeTransforms.horizontallyFlippedCopyOf(leftSide);
+       
+        // after flipping around the upper left hand corner of the
+        // bounding box, we move this over to the right by 200 pixels
+       
+        rightSide = ShapeTransforms.translatedCopyOf(rightSide, 200.0, 0.0);
+       
+        // now we put the whole thing together ino a single path.
+       
+        GeneralPath wholeRocket = new GeneralPath ();
+        wholeRocket.append(topAndBottom, false);
+        wholeRocket.append(leftSide, false);
+        wholeRocket.append(rightSide, false);
+        wholeRocket.append(leftFin, false);
+        wholeRocket.append(rightFin, false);
+        wholeRocket.append(topOfRocket, false);
+        
+
+        // translate to the origin by subtracting the original upper left x and y
+        // then translate to (x,y) by adding x and y
+        
+        Shape s = ShapeTransforms.translatedCopyOf(wholeRocket, -ORIG_ULX + x, -ORIG_ULY + y);
+ 
+	// scale to correct height and width
+        s =  ShapeTransforms.scaledCopyOf(s,
+					  width/ORIG_WIDTH,
+					  height/ORIG_HEIGHT) ;
+	 
+	// Use the GeneralPath constructor that takes a shape and returns
+	// it as a general path to set our instance variable cup
+        
+	this.set(new GeneralPath(s));
+        
+    }
+
+}
