e24bc80014e2852abfadee3c21f8ba230672d167
Colin Garrett <colingarrett@umail.ucsb.edu>
2016-10-27 11:04:43 -0700

    finished drawing

---
+++ src/edu/ucsb/cs56/drawings/colingarrett/advanced/JackOLantern.java
@@ -0,0 +1,46 @@
+package edu.ucsb.cs56.drawings.colingarrett.advanced;
+import java.awt.geom.GeneralPath;  
+import java.awt.Shape; 
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
+
+/**
+   A JackOLantern (i.e. a Pumpkin with a face)
+      
+   @author Phill Conrad 
+   @author Colin Garrett
+   @version for CS56, F16, UCSB
+   
+*/
+public class JackOLantern extends Pumpkin implements Shape
+{
+    /**
+     * Constructor for objects of class JackOLantern
+     */
+    public JackOLantern(double x, double y, double width, double height)
+    {
+	// construct the basic pumpkin shell
+	super(x,y,width,height);
+	
+	// get the GeneralPath that we are going to carve--append--into
+	GeneralPath gp = this.get();
+	
+	double w = 0.20 * width;
+	double h = 0.20 * 0.75 * height;
+	double eyeTop =  y + (0.25 * height) + h;
+
+	// make eyes and mouth
+	Ellipse2D.Double leftEye =
+	    new Ellipse2D.Double(x + w, eyeTop, w, h);
+	Ellipse2D.Double rightEye =
+	    new Ellipse2D.Double(x + 3.0*w, eyeTop, w, h);
+	Rectangle2D.Double mouth =
+	    new Rectangle2D.Double(x + w, eyeTop + 2.0*h, 3.0*w, h);
+	
+	// add the eyes and mouth to the pumpkin
+        GeneralPath wholePumpkin = this.get();
+        wholePumpkin.append(leftEye, false);
+        wholePumpkin.append(rightEye, false);
+        wholePumpkin.append(mouth, false); 
+    }    
+}
---
+++ src/edu/ucsb/cs56/drawings/colingarrett/advanced/Pumpkin.java
@@ -0,0 +1,57 @@
+package edu.ucsb.cs56.drawings.colingarrett.advanced;
+import java.awt.geom.GeneralPath;
+import java.awt.Shape;
+
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
+
+import edu.ucsb.cs56.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
+
+/**
+   A vector drawing of a house that implements
+   the Shape interface, and so can be drawn, as well as
+   rotated, scaled, etc.
+      
+   @author Phill Conrad 
+   @author Colin Garrett
+   @version for CS56, F16, UCSB
+   
+*/
+public class Pumpkin extends GeneralPathWrapper implements Shape
+{
+    /**
+       Constructor
+       
+       @param x x coord of lower left corner of pumpkin
+       @param y y coord of lower left corner of pumpkin
+       @param width width of the pumpkin
+       @param height of pumpkin (including circle and stem)
+    */
+    public Pumpkin(double x, double y, double width, double height)
+    {
+	
+        double stemHeight = .25 * height;
+        double circleHeight = height - stemHeight;
+        double circleUpperLeftY = y + stemHeight;
+        
+        // make the pumpkin without the stem
+        
+        Ellipse2D.Double theCircle =
+	    new Ellipse2D.Double(x, circleUpperLeftY,
+				 width, circleHeight);
+            	
+        // make the stem
+
+	double w = 0.2 * width;
+	
+	Rectangle2D.Double theStem =
+	    new Rectangle2D.Double(x+2*w, y, w, stemHeight*1.1);
+	
+        // put the whole pumpkin together
+	
+        GeneralPath wholePumpkin = this.get();
+        wholePumpkin.append(theCircle, false);
+        wholePumpkin.append(theStem, false);
+    }
+}
