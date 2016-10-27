09967c43b83de6588528344d45cb80946df722f7
Jordan Ang <jordanangof18@gmail.com>
2016-10-26 20:28:41 -0700

    Jordan's graphics created

--- src/edu/ucsb/cs56/drawings/jordanang/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/drawings/jordanang/advanced/AllMyDrawings.java
@@ -51,12 +51,8 @@ public class AllMyDrawings
 	h3 = ShapeTransforms.rotatedCopyOf(h3, Math.toRadians(-30));
 	g2.setColor(Color.RED); g2.draw(h3);
 	
-	// We'll draw this with a thicker stroke
+	//thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
 	
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
@@ -64,19 +60,15 @@ public class AllMyDrawings
 	g2.draw(h2); 
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
 	g2.drawString("Power Bombers by Jordan Ang", 20,20);
     }
     
-    
-    /** Draw a picture with a bombs and Bob-ombs
-     */
+  
     public static void drawPicture2(Graphics2D g2) {
 	
 	// Draw some Bob-ombs
-	
 	BobOmb large = new BobOmb(100,50,225);
 	BobOmb smallCC = new BobOmb(20,50,40);
 	BobOmb tallSkinny = new BobOmb(20,150,20);
@@ -101,12 +93,8 @@ public class AllMyDrawings
 	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
 	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
 	
-	// We'll draw this with a thicker stroke
+	//thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
 	
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
@@ -127,19 +115,14 @@ public class AllMyDrawings
 	g2.draw(hw3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
 	g2.drawString("If Bob-Omb had a Music Video by Jordan Ang", 20,20);
     }
     
-    /** Draw a different picture with a few houses and coffee cups
-     */
-    
     public static void drawPicture3(Graphics2D g2) {
 	
 	// label the drawing
-	
 	g2.drawString("King Bob-omb and Friends by Jordan Ang", 20,20);
 	
 	//Draw a large bob-omb
