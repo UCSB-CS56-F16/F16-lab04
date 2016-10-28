0ab82aa083393a67ffffa2319901e5c96938d2e4
Xinyuan <xinyuanzhang@umail.ucsb.edu>
2016-10-28 16:43:28 -0700

    added updated version of SimpleGUI1 to my src directory - done with Step 5

--- src/edu/ucsb/cs56/drawings/xinyuanzhang/SimpleGui1.java
+++ src/edu/ucsb/cs56/drawings/xinyuanzhang/SimpleGui1.java
@@ -7,7 +7,7 @@ import javax.swing.*;
     
     @author Head First Java, 2nd Edition p. 355
     @author P. Conrad (who only typed it in and added the Javadoc comments)
-    @author TODO: Add additional author here
+    @author Xinyuan Zhang
     @version CS56, Spring 2013, UCSB
 */
 
@@ -20,9 +20,9 @@ public class SimpleGui1 {
     public static void main (String[] args) {
 	JFrame frame = new JFrame() ;
 	
-	JButton button = new JButton("Click me") ;
+	JButton button = new JButton("majic button") ;
 	
-	java.awt.Color myColor = new java.awt.Color(204,255,000);   // R, G, B values.
+	java.awt.Color myColor = new java.awt.Color(199,255,000);   // R, G, B values.
 	button.setBackground(myColor);
 	button.setOpaque(true);
 	
