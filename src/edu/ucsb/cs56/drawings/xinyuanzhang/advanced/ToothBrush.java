49e383817ac11a57cd03d340ca236a06e55e254b
Xinyuan Zhang <xinyuanzhang@uamil.ucsb.edu>
2016-10-29 13:23:25 -0700

    toothbrush finished

--- src/edu/ucsb/cs56/drawings/xinyuanzhang/advanced/ToothBrush.java
+++ src/edu/ucsb/cs56/drawings/xinyuanzhang/advanced/ToothBrush.java
@@ -1,10 +1,11 @@
 package edu.ucsb.cs56.drawings.xinyuanzhang.advanced;
-import java.awt.geom.GeneralPath;
+
 import java.awt.Shape;
-
+import java.awt.Rectangle;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
-import java.wat.geom.Recantagle2D;
-
+import java.awt.geom.Recantagle2D;
+import java.awt.geom.PathInerator;
 import edu.ucsb.cs56.drawings.utilities.ShapeTransforms;
 import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
 
@@ -29,7 +30,7 @@ public class ToothBrush extends GeneralPathWrapper implements Shape
        @param height of toothbrush
     */
 
-    public House(double x,double y,double width,double height)
+    public ToothBrush(double x,double y,double width,double height)
     { 
 	double tHeadHeight = 0.2 * height;
 	double tHeadWidth = 0.8*width;
@@ -38,19 +39,22 @@ public class ToothBrush extends GeneralPathWrapper implements Shape
 
 	//Make teh first toothbrush
 
-	Rectangle2D.Double firstTb =
+	Rectangle2D.Double tNeck =
 	    new Rectangle2D.Double(x,y,tNeckWidth,height);
 
 	//make the head
 	Rectangle2D.Double tHead =
-	    new Rectangle2D.Double(x+tNeckWidth,y,tHeadWidth,tHeadHeight);
-	Rectangle2D.Double tHead1;
-	Rectangle2D.Double tHead2 ;
-	Rectangle2D.Double tHead3;
-	Rectangle2D.Double tHead4;
-	Rectangle2D.Double tHead5;
-	//for(int i=1;i<5 ;i++){
-	    
+	    new Rectangle2D.Double(x+tNeckWidth,0.95*y,tHeadWidth,tHeadHeight);     
+	Rectangle2D.Double [] tHeads = new Rectangle2D.Double[5] ;
+	for(double i=0;i<5 ;i++)
+	    tHeads[i]= new Rectangle2D.Double(x+tNeckWidth,0.95*y-0.2*(i+1),tHeadWidth,lineSpace);
+
+	//put the all components together
+	GeneralPath toothBrush = this.get();
+	toothBrush.append(tNeck,false);
+	toothBrush.append(tHead,false);
+	for(int j=0;j<5;j++)
+	    toothBrush.append(tHeads[j],false);
     }
 }
 
---
+++ src/edu/ucsb/cs56/drawings/xinyuanzhang/advanced/ToothBrushWithCup.java
@@ -0,0 +1,51 @@
+package edu.ucsb.cs56.drawings.xinyuanzhang.advanced;
+
+import java.awt.Shape;
+import java.awt.Rectangle;
+import java.awt.geom.GenaralPath;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.PathIterator;
+
+import edu.ucsb.cs56.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
+
+
+/**
+   @author Xinyuan Zhang
+
+   @version for CS56,F16,UCSB
+*/
+
+Public class ToothBrushWithCup extends ToothBrush implements Shape{
+
+    /**
+       Constructor
+       @param x the coordinate of the left edge
+       @param y the coordinate of the top
+       @param width width of the ToothBrush 
+       @param height  height of the ToothBrush
+    */
+
+    public ToothBrushWithCup (double x, double y,double width, double height){
+	super(x,y,width,height);
+
+	Point2D.Double upper = new Point2D.Double(x,0.5*y);
+	Point2D.Double lower = new Point2D.Double(x,0);
+	Point2D.Double right = new Point2D.Double(x+0.2*y,0.4y);
+	Point2D.Double left = new Point2D.Double(x-0.2y,0.1y);
+	
+	Line2D.Double cupTop = new Line2D.Double(upper,right);
+	Line2D.Double cupRight = new Line2D.Double(right,lower);
+	Line2D.Double cupBottom = new Line2D.Double(lower,left);
+	Line2D.Double cupLeft = new Line2D.Double(left,upper);
+
+	//put all components together
+	GeneralPath toothBrushWithCup = this.get();
+	toothBrushWithCup.append(cupTop, false);
+	toothBrushWithCup.append(cupRight, false);
+	toothBrushWithCup.append(cupBottom, false);
+	toothBrushWithCup.append(cupLeft, false);
+    }
+}
