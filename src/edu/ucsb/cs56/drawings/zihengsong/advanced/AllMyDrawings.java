e8c01b5695cced1416382d44dc9ff17565e5d126
Ziheng Song <zihengsong@umail.ucsb.edu>
2016-10-29 12:18:49 -0700

    generated javadoc

--- src/edu/ucsb/cs56/drawings/zihengsong/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/drawings/zihengsong/advanced/AllMyDrawings.java
@@ -6,37 +6,40 @@ import java.awt.Color; // class for Colors
 import java.awt.Stroke;
 import java.awt.BasicStroke;
 
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
 import edu.ucsb.cs56.drawings.utilities.ShapeTransforms;
 import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
 
 /**
  * A class with static methods for drawing various pictures
  * 
- * @author Phill Conrad 
+ * @author Phill Conrad, Ziheng Song 
  * @version for UCSB CS56, W16 
  */
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses 
+    /** Draw a picture with a few skateboards 
      */
     
     public static void drawPicture1(Graphics2D g2) {
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Skateboard s1 = new Skateboard(100,250,100,10);
+	g2.setColor(Color.CYAN); g2.draw(s1);
 	
-	// Make a black house that's half the size, 
+	// Make a black skateboard that's half the size, 
 	// and moved over 150 pixels in x direction
 	
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape s2 = ShapeTransforms.scaledCopyOfLL(s1,0.5,0.5);
+	s2 = ShapeTransforms.translatedCopyOf(s2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(s2);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a skateboard that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	s2 = ShapeTransforms.scaledCopyOfLL(s2,4,4);
+	s2 = ShapeTransforms.translatedCopyOf(s2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -48,53 +51,53 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(s2); 
 	
 	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	DoubleSlideSkateboard ds1 = new DoubleSlideSkateboard(50,350,40,4);
+	DoubleSlideSkateboard ds2 = new DoubleSlideSkateboard(200,350,200,20);
 	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
+	g2.draw(ds1);
+	g2.setColor(new Color(0x8F00FF)); g2.draw(ds2);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+	g2.drawString("A few skateboardss by Ziheng Song", 20,20);
     }
     
     
-    /** Draw a picture with a few houses and coffee cups
+    /** Draw a picture with a few skateboards and double slide skateboards
      */
     public static void drawPicture2(Graphics2D g2) {
 	
 	// Draw some coffee cups.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
+	Skateboard large = new Skateboard(100,50,300,30);
+	Skateboard small = new Skateboard(20,50,50,5);
+	Skateboard tallShort = new Skateboard(20,150,200,50);
+	Skateboard shortLong = new Skateboard(20,450,700,30);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
-	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
-	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
+	g2.setColor(Color.GREEN);   g2.draw(small);
+	g2.setColor(Color.BLUE);    g2.draw(tallShort);
+	g2.setColor(Color.MAGENTA); g2.draw(shortLong);
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Skateboard s1 = new Skateboard(100,250,50,5);
+	g2.setColor(Color.CYAN); g2.draw(s1);
 	
-	// Make a black house that's half the size, 
+	// Make a black skateboard that's half the size, 
 	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape s2 = ShapeTransforms.scaledCopyOfLL(s1,0.5,0.5);
+	s2 = ShapeTransforms.translatedCopyOf(s2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(s2);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a skateboard that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	s2 = ShapeTransforms.scaledCopyOfLL(s2,4,4);
+	s2 = ShapeTransforms.translatedCopyOf(s2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -106,45 +109,45 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(s2); 
 	
 	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	DoubleSlideSkateboard ds1 = new DoubleSlideSkateboard(50,350,40,4);
+        DoubleSlideSkateboard ds2 = new DoubleSlideSkateboard(200,350,200,20);
 	
-	g2.draw(hw1);
+	g2.draw(ds1);
 	g2.setColor(new Color(0x8F00FF)); 
 	
-	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+	// Rotate the second skateboard 45 degrees around its center.
+	Shape ds3 = ShapeTransforms.rotatedCopyOf(ds2, Math.PI/4.0);
 	
-	g2.draw(hw3);
+	g2.draw(ds3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("A bunch of skateboards and double slide skateboardsby Ziheng Song", 20,20);
     }
     
-    /** Draw a different picture with a few houses and coffee cups
+    /** Draw a different picture with a few skateboards
      */
     
     public static void drawPicture3(Graphics2D g2) {
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+	g2.drawString("A bunch of skateboards by Ziheng Song", 20,20);
 	
 	
-	// Draw some coffee cups.
+	// Draw some skateboards.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+	Skateboard large = new Skateboard(100,50,300,30);
+	Skateboard small = new Skateboard(20,50,60,6);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
+	g2.setColor(Color.GREEN);   g2.draw(small);
 	
     }       
 }
