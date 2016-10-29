9d010ed0cd78bf457716968623a693239b0946e8
thatonesimon <thatonesimon@gmail.com>
2016-10-29 10:48:12 -0700

    Update AllMyDrawings.java

--- src/edu/ucsb/cs56/drawings/sswong/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/drawings/sswong/advanced/AllMyDrawings.java
@@ -153,8 +153,8 @@ public class AllMyDrawings
 	g2.drawString("Tree trees by Simon Wong",20,20);
 
 	// draw three trees
-	Tree t1 = new Tree(100,50,100,25,50);
-	Tree t2 = new Tree(200,50,75,20,30);
+	Tree t1 = new Tree(0,50,100,25,50);
+	Tree t2 = new Tree(200,75,75,20,30);
 	Tree t3 = new Tree(100,200,50,40,25);
 	g2.setColor(Color.GREEN);
 	g2.draw(t1);
@@ -167,8 +167,8 @@ public class AllMyDrawings
 	g2.drawString("Three trees with fruits by Simon Wong",20,20);
 
 	// draw three trees with fruits
-	TreeWithFruits twf1 = new TreeWithFruits(100,50,100,25,50);
-	TreeWithFruits twf2 = new TreeWithFruits(200,50,75,20,30);
+	TreeWithFruits twf1 = new TreeWithFruits(0,50,100,25,50);
+	TreeWithFruits twf2 = new TreeWithFruits(200,75,75,20,30);
 	TreeWithFruits twf3 = new TreeWithFruits(100,200,50,40,25);
 	g2.setColor(Color.GREEN);
 	g2.draw(twf1);
