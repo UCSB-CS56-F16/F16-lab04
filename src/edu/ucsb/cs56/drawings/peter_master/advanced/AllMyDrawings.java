9ba01d46ca3d81328d26b393bcdd0b837813b154
Peter <peter_master@umail.ucsb.edu>
2016-10-30 22:05:37 -0700

    implement AllMyDrawings.java

--- src/edu/ucsb/cs56/drawings/peter_master/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/drawings/peter_master/advanced/AllMyDrawings.java
@@ -13,12 +13,12 @@ import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
  * A class with static methods for drawing various pictures
  * 
  * @author Peter Master
- * @version for UCSB CS56, W16 
+ * @version for UCSB CS56, F16 
  */
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses 
+    /** Draw a picture with a few stick figures
      */
     
     public static void drawPicture1(Graphics2D g2) {
@@ -26,31 +26,19 @@ public class AllMyDrawings
 	StickFigure h1 = new StickFigure(100,250,50,75);
 	g2.setColor(Color.CYAN); g2.draw(h1);
 	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
-	
 	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
 	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
 	g2.setColor(Color.BLACK); g2.draw(h2);
-	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
+
 	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
 	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	
-	// We'll draw this with a thicker stroke
+
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
 	
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
 	g2.draw(h2); 
-	
-	// Draw two houses with Windows
 	
 	FashionableStickFigure hw1 = new FashionableStickFigure(50,350,40,75);
 	FashionableStickFigure hw2 = new FashionableStickFigure(200,350,200,100);
@@ -58,11 +46,10 @@ public class AllMyDrawings
 	g2.draw(hw1);
 	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
 	
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+	g2.drawString("A bunch of stick figures by Peter Master", 20,20);
     }
     
     
@@ -70,78 +57,62 @@ public class AllMyDrawings
      */
     public static void drawPicture2(Graphics2D g2) {
 	
-	// Draw some coffee cups.
+	// Draw some stick figures.
 	
-	StickFigure large = new StickFigure(100,50,225,150);
-	StickFigure smallCC = new StickFigure(20,50,40,30);
-	StickFigure tallSkinny = new StickFigure(20,150,20,40);
-	StickFigure shortFat = new StickFigure(20,250,40,20);
+	StickFigure giant = new StickFigure(100,50,225);
+	StickFigure small1 = new StickFigure(20,50,40);
+	StickFigure tiny = new StickFigure(20,150,20);
+	StickFigure small2 = new StickFigure(20,250,40);
 	
-	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
-	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
-	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
+	g2.setColor(Color.RED);     g2.draw(giant);
+	g2.setColor(Color.GREEN);   g2.draw(small1);
+	g2.setColor(Color.BLUE);    g2.draw(tiny);
+	g2.setColor(Color.MAGENTA); g2.draw(small2);
 	
 	StickFigure h1 = new StickFigure(100,250,50,75);
 	g2.setColor(Color.CYAN); g2.draw(h1);
 	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
 	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
 	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
 	g2.setColor(Color.BLACK); g2.draw(h2);
 	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
 	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
 	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
 	
-	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
 	
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
 	g2.draw(h2); 
 	
-	// Draw two houses with Windows
-	
 	FashionableStickFigure hw1 = new FashionableStickFigure(50,350,40,75);
 	FashionableStickFigure hw2 = new FashionableStickFigure(200,350,200,100);
 	
 	g2.draw(hw1);
 	g2.setColor(new Color(0x8F00FF)); 
-	
-	// Rotate the second house 45 degrees around its center.
 	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
 	
 	g2.draw(hw3);
 	
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("A bunch of stick figures, both with hats and not, by Peter Master", 20,20);
     }
     
-    /** Draw a different picture with a few houses and coffee cups
+    /** Draw a different picture with a stick
+        figure with a hat and a stick figure without
      */
     
     public static void drawPicture3(Graphics2D g2) {
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of stick figures by Peter Master", 20,20);
-	
-	
-	// Draw some coffee cups.
+	g2.drawString("A couple of stick figures by Peter Master", 20,20);
 	
 	StickFigure hatlessLargePerson = new StickFigure(100,50,225);
-	FashionableStickFigure hattedSmallPerson = new FashionableStickFigure(20,50,40,30);
+	FashionableStickFigure hattedSmallPerson = new FashionableStickFigure(20,50,225,90);
 	
 	g2.setColor(Color.RED);     g2.draw(hatlessLargePerson);
 	g2.setColor(Color.GREEN);   g2.draw(hattedSmallPerson);
