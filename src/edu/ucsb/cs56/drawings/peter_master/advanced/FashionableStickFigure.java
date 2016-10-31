1e79460c24b3acde48903b6b018fff2316070cc0
Peter <peter_master@umail.ucsb.edu>
2016-10-30 21:57:38 -0700

    implement StickFigure, FashionableStickFigure, and everything that is related to the s imple directory

---
+++ src/edu/ucsb/cs56/drawings/peter_master/advanced/FashionableStickFigure.java
@@ -0,0 +1,42 @@
+package edu.ucsb.cs56.drawings.peter_master.advanced;
+import java.awt.geom.GeneralPath;  
+import java.awt.Shape; 
+import java.awt.geom.Rectangle2D;
+
+/**
+   A House
+      
+   @author Peter Master 
+   @version for CS56, F16, UCSB
+   
+*/
+public class FashionableStickFigure extends StickFigure implements Shape
+{
+    /**
+     * Constructor for objects of class FashionableStickFigure
+     */
+    public FashionableStickFigure(double x, double y, double height, double hatHeight) {}
+	// construct the stick figure
+	super(x,y,height);
+	
+	// get the GeneralPath that we are going to append stuff to
+	GeneralPath gp = this.get();
+
+	double hatBrimWidth = hatHeight * .6;
+	double hatTopWidth = hatBrimWidth * .7;
+	
+	Rectangle2D.Double hatTop =
+	    new Rectangle2D.Double (x - hatTopWidth/2, y,
+	    						x + hatTopWidth/2, y + hatHeight);
+
+	Line2D.Double hatBrim = 
+		new Line2D.Double (x - hatBrimWidth/2, y,
+							x + hatBrimWidth/2, y);
+
+	// put the whole stick figure together
+    GeneralPath stickFigure = this.get();
+    stickFigure.append(win1, false);
+    stickFigure.append(win2, false);
+    stickFigure.append(win3, false); 
+    }    
+}
