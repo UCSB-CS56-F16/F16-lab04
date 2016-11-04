905f44e159c17a3e2c6cedf9bd9f34cb9552af5e
Zhancheng Qian <zhanchengqian@umail.ucsb.edu>
2016-11-04 10:42:36 -0700

    klong changed

--- src/edu/ucsb/cs56/drawings/klong/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/drawings/klong/advanced/AllMyDrawings.java
@@ -68,32 +68,30 @@ public class AllMyDrawings
     /** Draw a picture with a few houses and coffee cups
      */
     public static void drawPicture2(Graphics2D g2) {
-	
-	// Draw some coffee cups.
-	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
+  
+	TShirt large = new TShirt(150,150,100,100,30,30);
+	TShirt small = new TShirt(30,30,25,25,8,8);
+	TShirt tallSkinny = new TShirt(40,40,20,50,10,10);
+	TShirt shortFat = new TShirt(180,180,50,20,10,15);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
+	g2.setColor(Color.GREEN);   g2.draw(small);
 	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
 	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	TShirt t1 = new TShirt(30,30,25,25,10,10);
+	g2.setColor(Color.CYAN); g2.draw(t1);
 	
 	// Make a black house that's half the size, 
 	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape t2 = ShapeTransforms.scaledCopyOfLL(t1,0.5,0.5);
+	t2 = ShapeTransforms.translatedCopyOf(t2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(t2);
 	
 	// Here's a house that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	t2 = ShapeTransforms.scaledCopyOfLL(t2,4,4);
+	t2 = ShapeTransforms.translatedCopyOf(t2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -105,12 +103,12 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(t2); 
 	
 	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	TShirtWithLogo hw1 = new TShirtWithLogo(150,150,50,50,7,7);
+	TShirtWithLogo hw2 = new TShirtWithLogo(300,300,50,50,7,7);
 	
 	g2.draw(hw1);
 	g2.setColor(new Color(0x8F00FF)); 
@@ -124,7 +122,7 @@ public class AllMyDrawings
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("A bunch of T-Shirts by Keith Long", 20,20);
     }
     
     /** Draw a different picture with a few houses and coffee cups
@@ -134,16 +132,14 @@ public class AllMyDrawings
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+	g2.drawString("A bunch of T- Shirts by Keith Long", 20,20);
 	
-	
-	// Draw some coffee cups.
-	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	
+	// Draw some coffee cups.	
+	TShirt large = new TShirt(150,150,50,50,15,15);
+	TShirt small = new TShirt(40,40,25,25,8,8);
+
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
+	g2.setColor(Color.GREEN);   g2.draw(small);
 	
     }       
 }
